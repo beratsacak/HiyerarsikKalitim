12588a9814a7e294a277433a3566f9c52430cba2
berat584 <berat584@BPC08>
2023-12-14 15:17:57 +0300

    Proje dosyası ekle.

---
+++ Canlılar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiyerarkaşik_kalitim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Canli
+    {
+        public string Beslenme { get; set; }
+        public string Nefes { get; set; }
+    }
+    public class Hayvan : Canli
+    {
+        public string hareket { get; set; }
+    }
+    public class Köpek : Hayvan
+    {
+        public string HAWHAW { get; set; }
+    }
+    public class Kangal : Köpek
+    {
+        public string Guclu { get; set; }
+    }
+
+}
